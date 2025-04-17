5eba0d4f970e9601cb2c9b4fa752a50efddf70ce
chighwood <hig23003@byui.edu>
2025-04-17 16:39:05 -0500

    New

--- backend/server.cjs
+++ backend/server.cjs
@@ -1,14 +1,10 @@
 const express = require('express');
 const path = require('path');
-const { fileURLToPath } = require('url');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Go up to root of project
+// Path to the root of the project
 const rootDir = path.join(__dirname, '..');
 
 app.use(express.static(rootDir));
