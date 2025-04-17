2d52a0adb77571b76210458bf5787ffba6e54d02
chighwood <hig23003@byui.edu>
2025-04-16 22:27:45 -0500

    ?

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.get('/api/:proxyPath(*)', async (req, res) => {
-    const proxyPath = req.params.proxyPath || '';
+app.get('/api/*', async (req, res) => {
+    const proxyPath = req.params[0];
     const url = `https://www.carqueryapi.com/${proxyPath}${req.url.split(proxyPath).slice(1).join('')}`;
-  
     try {
       const response = await fetch(url);
       const data = await response.text();
@@ -19,7 +18,8 @@ app.get('/api/:proxyPath(*)', async (req, res) => {
       console.error('Proxy error:', error);
       res.status(500).send('Error fetching data from external API');
     }
-  });  
+  });
+  
 
 app.listen(PORT, () => {
   console.log(`Proxy server running on port ${PORT}`);
